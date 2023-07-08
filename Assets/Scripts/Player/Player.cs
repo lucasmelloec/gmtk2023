b2ec56a8d713ae248a46438a2401ab599928b0ac
Lucas Mello <lucasmelloec@gmail.com>
2023-07-08 01:52:33 -0300

    feat: input map

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Player : MonoBehaviour {
+  [SerializeField]
+  private float moveSpeed = 7f;
+  private PlayerInputActions playerInputActions;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  private void Awake() {
+    playerInputActions = new PlayerInputActions();
+    playerInputActions.PlayerInput.Enable();
+  }
+
+  private void Update() {
+    HandleMovement();
+  }
+
+  private void HandleMovement() {
+    Vector2 inputVector = playerInputActions.PlayerInput.Move.ReadValue<Vector2>();
+    inputVector = inputVector.normalized;
+    float moveDistance = moveSpeed * Time.deltaTime;
+    transform.Translate(moveDistance * inputVector);
+  }
 }
